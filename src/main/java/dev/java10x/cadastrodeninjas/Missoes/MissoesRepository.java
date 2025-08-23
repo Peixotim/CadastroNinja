f2e4b51e953e970bd839411ee322ed8ceb3d022b
Peixotim <pedropeixotovz@gmail.com>
2025-08-22 21:47:45 -0300

    fix : Adiciona a interface MissoesModel que extende a JpaRepository

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesRepository.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesRepository.java
@@ -1,4 +1,6 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
-public class MissoesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+//Aqui a gente passa onde está o model da instancia e o tipo do meu ID , no caso dessa aplicacao long
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long>{
 }
