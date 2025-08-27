db4409b3fa1ebd1307910ed51173be191d6ba8da
Peixotim <pedropeixotovz@gmail.com>
2025-08-27 15:31:47 -0300

    feat:Cria as rotas do MissaoController

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissaoController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissaoController.java
@@ -1,4 +1,35 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissaoController {
+
+
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Criando Missoes";
+    }
+
+
+    @GetMapping("/todos")
+    public String mostrarTodosAsMissoes(){
+        return "Mostrando todos as Missoes";
+    }
+
+    @GetMapping("/todosId")
+    public String mostrarTodasAsMissoesPorId(){
+        return "Mostrando todas as Missoes por ID";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissoesPorId(){
+        return "Alterando Missoes por ID";
+    }
+
+    @DeleteMapping("/deletarMissao")
+    public String deletarMissoes(){
+        return  "Deletando Ninja";
+    }
 }
--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -1,15 +1,42 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping 
+@RequestMapping ("ninja")
 public class NinjaController {
 
     @GetMapping("/boasvindas") //E aqui que passa o valor localhost:8080/boasVindas
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota!";
     }
+
+   //FAZER O CRUD AGORA CREATE READ UPDATE E DELETE
+
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Criando Ninja";
+    }
+
+
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrando Ninja";
+    }
+
+    @GetMapping("/todosId")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Mostrando Ninja";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarNinjaPorId(){
+        return "Alterando Ninja";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarNinjaPorId(){
+        return  "Deletando Ninja";
+    }
+
 }
